89aa906b197b43ad410a39ee038f7f363567a4cc
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-16 14:53:49 +0530

    basic setup done

---
+++ apps/ws-server/index.ts
@@ -0,0 +1,22 @@
+import { WebSocketServer } from "ws";
+import { client } from "@repo/db/client"
+
+interface IUser{
+    username:string;
+    password:string;
+}
+
+
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection',async (socket)=>{
+    const user = await client.user.create({
+        data:{
+            username : Math.random().toString(),
+            password : Math.random().toString()
+        }
+    })
+    const userId = user.id;
+
+    socket.send(`hello user ${userId}`)
+})
